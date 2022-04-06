100c788663603f3aaea7e882beda910d74b0678a
JoharryJC <energycuenta@yahoo.com>
2022-04-06 22:04:41 +0200

    new implementations

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -3,8 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    .container {
+      margin: 10px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
